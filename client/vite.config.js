2e17429c01cddf60f4cfa0ac9c686acd7ee7b7ee
shubhanshu2003 <chaubeyshubhanshu30@gmail.com>
2025-08-04 14:27:05 +0530

    so

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
+  base: '/',              // <- set this!
   build: {
     chunkSizeWarningLimit: 1000,
-    outDir: 'build',
+    outDir: 'build'
   }
-})
+});
 
